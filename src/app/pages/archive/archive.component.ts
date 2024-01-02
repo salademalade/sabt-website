fcaeade8cb022e5e9fd29c672e7bcaf55102c7ab
Woojin Son <woojin.son@web.de>
2024-01-02 09:32:27 +0100

    Add archive pagination

--- src/app/pages/archive/archive.component.ts
+++ src/app/pages/archive/archive.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { Firestore, Timestamp, collection, getDocs, orderBy, query, where } from '@angular/fire/firestore';
+import { Firestore, Timestamp, collection, getCountFromServer, getDocs, limit, orderBy, query, sum, where } from '@angular/fire/firestore';
+import { Storage, getDownloadURL, ref } from '@angular/fire/storage';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-archive',
@@ -11,26 +13,38 @@ import { Firestore, Timestamp, collection, getDocs, orderBy, query, where } from
 })
 export class ArchiveComponent {
   firestore: Firestore = inject(Firestore);
+  storage: Storage = inject(Storage);
+  route: ActivatedRoute = inject(ActivatedRoute);
 
-  data: { title: string, chapters: { title: string, number: number, imageURL: string }[] }[] = [];
+  chapter: number = 0;
+  count: number = 0;
 
-  ngOnInit() {
-    getDocs(query(collection(this.firestore, 'chapters'), orderBy('number', 'asc'))).then((snap) => {
-      snap.forEach((docSnap) => {
-        const title: string = docSnap.data()['title'];
-        let chapters: { title: string, number: number, imageURL: string }[] = [];
-        getDocs(query(collection(this.firestore, 'chapters', docSnap.id, 'pages'), where('createdAt', '<=', Timestamp.now()), orderBy('createdAt', 'asc'), orderBy('number', 'asc'))).then((snap) => {
-          snap.forEach((docSnap) => {
-            const title: string = docSnap.data()['titleShort'];
-            const number: number = docSnap.data()['number'];
-            const imageURL: string = docSnap.data()['imageURL'];
+  title: string = '';
 
-            chapters.push({ title, number, imageURL });
-          });
-        });
+  data: any[] = [];
+  chapters: number[] = [];
 
-        this.data.push({ title, chapters });
+  async ngOnInit() {
+    const ch = this.route.snapshot.queryParamMap.get('ch');
+    if (ch == null) this.chapter = 1;
+    else this.chapter = parseInt(ch);
+
+    this.count = (await getCountFromServer(collection(this.firestore, 'chapters'))).data().count;
+    this.chapters = Array(this.count).fill(0).map((x, i) => i + 1);
+
+    const chSnap = await getDocs(query(collection(this.firestore, 'chapters'), where('number', '==', this.chapter), limit(1)));
+
+    if (chSnap.docs.length < 1) return;
+
+    this.title = chSnap.docs[0].get('title');
+    const pgsSnap = await getDocs(query(collection(this.firestore, 'chapters', chSnap.docs[0].id, 'pages'), where('createdAt', '<=', Timestamp.now()), orderBy('createdAt', 'asc'), orderBy('number', 'asc')));
+
+    for (let doc of pgsSnap.docs) {
+      this.data.push({
+        title: doc.get('titleShort'),
+        number: doc.get('number'),
+        imageURL: await getDownloadURL(ref(this.storage, doc.get('imagePath'))),
       });
-    });
+    }
   }
 }
