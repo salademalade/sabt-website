957d48ef1bac6d01e17e1a0a0cdaa54e06fa3c1b
Woojin Son <woojin.son@web.de>
2024-01-02 14:49:44 +0100

    Add function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,19 +1,50 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { onRequest } from 'firebase-functions/v2/https';
+import { initializeApp } from 'firebase-admin/app';
+import { Timestamp, getFirestore } from 'firebase-admin/firestore';
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+const app = initializeApp();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const db = getFirestore(app);
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+export const genRSS = onRequest(async (req, res) => {
+  const snap = await db.collectionGroup('pages')
+    .where('createdAt', '<=', Timestamp.now())
+    .orderBy('createdAt', 'desc')
+    .orderBy('number', 'desc')
+    .limit(20)
+    .get();
+
+  let contentStr = '';
+  for (let doc of snap.docs) {
+    contentStr = contentStr.concat(
+      `
+      <item>
+        <title><![CDATA[${doc.get('titleLong')}]]></title>
+        <link>https://sniffandbarktales.com/comic/${doc.get('number').toString()}</link>
+        <pubDate><![CDATA[${doc.get('createdAt').toDate().toLocaleDateString('en-GB', {
+          weekday: 'short',
+          day: 'numeric',
+          month: 'short',
+          year: 'numeric'
+        })}]]></pubDate>
+        <description><![CDATA[${doc.get('description')}]]></description>
+      </item>
+      `
+    );
+  }
+
+  res.set('Content-Type', 'text/xml');
+  res.status(200).send(
+    `
+    <?xml version="1.0" encoding="UTF-8" ?>
+    <rss version="2.0">
+      <channel>
+        <title><![CDATA[Sniff and Bark Tales]]></title>
+        <link>https://sniffandbarktales.com</link>
+        <description><![CDATA[Sniff and Bark Tales Webcomic]]></description>
+        ${contentStr}
+      </channel>
+    </rss>
+    `.trim()
+  );
+});
