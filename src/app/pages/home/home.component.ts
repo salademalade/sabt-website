4ae9d46baff53582e48cd88b1836e3bfead78475
Woojin Son <woojin.son@web.de>
2024-01-05 08:05:22 +0100

    Add RSS button to homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { ComicDisplayComponent } from '../../components/comic-display/comic-display.component';
+import { faRss } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ComicDisplayComponent],
+  imports: [ComicDisplayComponent, FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  faRss = faRss;
 }
